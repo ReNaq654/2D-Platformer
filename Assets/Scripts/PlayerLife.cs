808d29f75d4cf5bfcbed3aa943624c4c8ccaa60f
ReNaq654 <121211738+ReNaq654@users.noreply.github.com>
2024-01-06 15:19:59 -0500

    Modified Level 1

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("DeathBlock"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        GetComponent<PlayerMovement>().enabled = false;
+        anim.SetTrigger("death");
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
