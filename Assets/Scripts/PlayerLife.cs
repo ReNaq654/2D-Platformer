bcfdd1947138a792e6f301ed46653a70d878692a
ReNaq654 <121211738+ReNaq654@users.noreply.github.com>
2024-01-06 18:06:16 -0500

    Modified PlayerLife and Player

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
     private Animator anim;
+    private Rigidbody2D rb;
+    private PlayerMovement movement;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        movement = GetComponent<PlayerMovement>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -23,7 +28,8 @@ public class PlayerLife : MonoBehaviour
 
     void Die()
     {
-        GetComponent<PlayerMovement>().enabled = false;
+        rb.bodyType = RigidbodyType2D.Static;
+        movement.enabled = false;
         anim.SetTrigger("death");
     }
 
