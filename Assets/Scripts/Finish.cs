8cf9c5c34a533432c4b98f406dbf03a630c04edc
ReNaq654 <121211738+ReNaq654@users.noreply.github.com>
2024-01-07 21:43:59 -0500

    Modified Level 1

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    bool levelCompleted = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+       if (collision.gameObject.name == "Player" && !levelCompleted)
+        {
+            levelCompleted = true;
+            Invoke(nameof(LoadLevel), 2f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadLevel()
+    {
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Start()
     {
         
     }
